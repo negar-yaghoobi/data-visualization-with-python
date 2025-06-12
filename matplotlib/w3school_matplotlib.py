1ae6a047aa51bdcf43cc06166b7831877c40f66e
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-06-12 10:11:23 +0330

    add example about Multiple Point : Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10) with plot()

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -33,3 +33,12 @@ ypoints = np.array([3, 10])
 
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
+
+
+# Multiple Points
+# Draw a line in a diagram from position (1, 3) to (2, 8) then to (6, 1) and finally to position (8, 10):
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
