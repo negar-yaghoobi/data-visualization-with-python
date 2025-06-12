5768eabee22302bf904ef71b1d02d8808b19675b
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-06-12 10:59:46 +0330

    add example about matplotlib marker

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -59,3 +59,36 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(ypoints, marker = 'o')
 plt.show()
+
+
+# Mark each point with a star:
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = '*')
+plt.show()
+
+## markers: 
+# 'o' 	Circle 	
+# '*' 	Star 	
+# '.' 	Point 	
+# ',' 	Pixel 	
+# 'x' 	X 	
+# 'X' 	X (filled) 	
+# '+' 	Plus 	
+# 'P' 	Plus (filled) 	
+# 's' 	Square 	
+# 'D' 	Diamond 	
+# 'd' 	Diamond (thin) 	
+# 'p' 	Pentagon 	
+# 'H' 	Hexagon 	
+# 'h' 	Hexagon 	
+# 'v' 	Triangle Down 	
+# '^' 	Triangle Up 	
+# '<' 	Triangle Left 	
+# '>' 	Triangle Right 	
+# '1' 	Tri Down 	
+# '2' 	Tri Up 	
+# '3' 	Tri Left 	
+# '4' 	Tri Right 	
+# '|' 	Vline 	
+# '_' 	Hline
