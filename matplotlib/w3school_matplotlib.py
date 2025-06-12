9165acec6529028718aee54c63f9ea0d482bece6
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-06-12 12:04:28 +0330

    add examples about marker color : set the color of the edge and the face to red

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -141,3 +141,10 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
 plt.show()
+
+
+# Set the color of both the edge and the face to red:
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
