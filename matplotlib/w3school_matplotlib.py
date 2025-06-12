65d3f3e1b111fcb41533287e4057fd2d034e50f7
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-06-12 09:50:28 +0330

    add example2 about Draw a line in a diagram from 2 positions with plot() and show()

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -16,3 +16,11 @@ ypoints = np.array([0, 250])
 
 plt.plot(xpoints, ypoints)      # plot()
 plt.show()                      # show()
+
+
+# Draw a line in a diagram from position (1, 3) to position (8, 10):
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
