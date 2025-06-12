5399138d00ce75c3c17ea2b84b752d22bd4fcbc6
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-06-12 10:03:56 +0330

    add example about Plotting Without Line : Draw two points in the diagram, one at position (1, 3) and one in position (8, 10): with plot() and show()

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -24,3 +24,12 @@ ypoints = np.array([3, 10])
 
 plt.plot(xpoints, ypoints)
 plt.show()
+
+
+# Plotting Without Line
+# Draw two points in the diagram, one at position (1, 3) and one in position (8, 10):
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+
+plt.plot(xpoints, ypoints, 'o')
+plt.show()
