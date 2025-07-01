0093fcff28371b02a671df7a565d4e292ddf6950
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 11:53:03 +0330

    Matplotlib Adding Grid Lines section

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -299,22 +299,25 @@ import numpy as np
 # font1 = {'family':'serif','color':'red','size':20}
 # font2 = {'family':'serif','color':'darkred','size':15}
 
-# plt.title("Sports Watch Data", fontdict = font1)
-# plt.xlabel("Average Pulse", fontdict = font2)
-# plt.ylabel("Calorie Burnage", fontdict = font2)
+# # plt.title("Sports Watch Data", fontdict = font1)
+# # plt.xlabel("Average Pulse", fontdict = font2)
+# # plt.ylabel("Calorie Burnage", fontdict = font2)
+
+# # plt.plot(x, y)
+# # plt.show()
+
+
+# # Position the Title
+# # Position the title to the left:
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title("Sports Watch Data", loc = 'left')
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
 
 # plt.plot(x, y)
 # plt.show()
 
 
-# Position the Title
-# Position the title to the left:
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-
-plt.title("Sports Watch Data", loc = 'left')
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-
-plt.plot(x, y)
-plt.show()
+# Matplotlib Adding Grid Lines
