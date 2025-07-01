703caba0dc87ada0e7d1d8d0069de015c402379b
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 10:15:49 +0330

    add example about multiplie lines

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -244,3 +244,13 @@ plt.plot(y1)
 plt.plot(y2)
 
 plt.show()
+
+
+# Draw two lines by specifiyng the x- and y-point values for both lines:
+x1 = np.array([0, 1, 3, 5, 7])
+y1 = np.array([0, 3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+
+plt.plot(x1, y1, x2, y2)
+plt.show()
