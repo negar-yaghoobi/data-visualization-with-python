f17830632416dcae6fdef5ace9597b451a50936e
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 11:14:25 +0330

    add example about create labels for a plot with xlabel() and ylabel() function

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -257,3 +257,17 @@ plt.show()
 
 
 # Matplotlib Labels and Title
+
+# Create Labels for a Plot
+# xlabel() and ylabel() function
+
+# Add labels to the x- and y-axis:
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
