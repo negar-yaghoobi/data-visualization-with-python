0416caba738c9eb1674c1796a8e2a42c01fa3b76
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 09:28:42 +0330

    add another example about linestyle

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -168,3 +168,9 @@ ypoints = np.array([3, 8, 1, 10])
 
 plt.plot(ypoints, linestyle = 'dotted')
 plt.show()
+
+
+# Use a dashed line:
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, linestyle = 'dashed')
+plt.show()
