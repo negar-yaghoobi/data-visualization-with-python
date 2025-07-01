acca56157c9967ca24639a706978d3f565e08b86
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 11:18:54 +0330

    add example about create title for a plot with title() function

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -230,30 +230,30 @@ import numpy as np
 # Line Width
 
 # # Plot with a 20.5pt wide line:
-# ypoints = np.array([3, 8, 1, 10])
-# plt.plot(ypoints, linewidth = '20.5')
+# # ypoints = np.array([3, 8, 1, 10])
+# # plt.plot(ypoints, linewidth = '20.5')
+# # plt.show()
+
+
+# # Multiple Lines
+# # Draw two lines by specifying a plt.plot() function for each line:
+# y1 = np.array([3, 8, 1, 10])
+# y2 = np.array([6, 2, 7, 11])
+
+# plt.plot(y1)
+# plt.plot(y2)
+
 # plt.show()
 
 
-# Multiple Lines
-# Draw two lines by specifying a plt.plot() function for each line:
-y1 = np.array([3, 8, 1, 10])
-y2 = np.array([6, 2, 7, 11])
+# # Draw two lines by specifiyng the x- and y-point values for both lines:
+# x1 = np.array([0, 1, 3, 5, 7])
+# y1 = np.array([0, 3, 8, 1, 10])
+# x2 = np.array([0, 1, 2, 3])
+# y2 = np.array([6, 2, 7, 11])
 
-plt.plot(y1)
-plt.plot(y2)
-
-plt.show()
-
-
-# Draw two lines by specifiyng the x- and y-point values for both lines:
-x1 = np.array([0, 1, 3, 5, 7])
-y1 = np.array([0, 3, 8, 1, 10])
-x2 = np.array([0, 1, 2, 3])
-y2 = np.array([6, 2, 7, 11])
-
-plt.plot(x1, y1, x2, y2)
-plt.show()
+# plt.plot(x1, y1, x2, y2)
+# plt.show()
 
 
 # Matplotlib Labels and Title
@@ -267,6 +267,23 @@ y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
 plt.plot(x, y)
 
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.show()
+
+
+
+# Create a Title for a Plot
+# title () function
+
+# Add a plot title and labels for the x- and y-axis:
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("Sports Watch Data")
 plt.xlabel("Average Pulse")
 plt.ylabel("Calorie Burnage")
 
