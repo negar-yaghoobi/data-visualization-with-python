cf16515fa130697486f825f0b9c0a4dbdf0cf759
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-01 11:48:36 +0330

    add example about position the title function

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -256,35 +256,65 @@ import numpy as np
 # plt.show()
 
 
-# Matplotlib Labels and Title
+# # Matplotlib Labels and Title
 
-# Create Labels for a Plot
-# xlabel() and ylabel() function
+# # Create Labels for a Plot
+# # xlabel() and ylabel() function
 
-# Add labels to the x- and y-axis:
+# # Add labels to the x- and y-axis:
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.show()
+
+
+
+# # Create a Title for a Plot
+# # title () function
+
+# # Add a plot title and labels for the x- and y-axis:
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.plot(x, y)
+
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.show()
+
+
+# # Set Font Properties for Title and Labels by use the fontdict parameter
+
+# # Set font properties for the title and labels:
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# font1 = {'family':'serif','color':'red','size':20}
+# font2 = {'family':'serif','color':'darkred','size':15}
+
+# plt.title("Sports Watch Data", fontdict = font1)
+# plt.xlabel("Average Pulse", fontdict = font2)
+# plt.ylabel("Calorie Burnage", fontdict = font2)
+
+# plt.plot(x, y)
+# plt.show()
+
+
+# Position the Title
+# Position the title to the left:
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.plot(x, y)
-
+plt.title("Sports Watch Data", loc = 'left')
 plt.xlabel("Average Pulse")
 plt.ylabel("Calorie Burnage")
 
-plt.show()
-
-
-
-# Create a Title for a Plot
-# title () function
-
-# Add a plot title and labels for the x- and y-axis:
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-
 plt.plot(x, y)
-
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-
 plt.show()
