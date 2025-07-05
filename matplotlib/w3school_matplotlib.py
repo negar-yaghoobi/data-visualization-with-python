e0c4441c3648810488df72d3e5d5d509866fd584
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 12:41:39 +0330

    add example about Set the line properties of the grid

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -370,3 +370,19 @@ plt.plot(x, y)
 plt.grid(axis = 'y')
 
 plt.show()
+
+
+# Set Line Properties for the Grid
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+
+plt.show()
