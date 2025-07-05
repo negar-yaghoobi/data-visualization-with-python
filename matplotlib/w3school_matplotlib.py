19ec284de0df5e18e91b72315f1aac89265f03e9
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 12:35:53 +0330

    add example about display only grid lines for the x-axis

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -324,8 +324,6 @@ import numpy as np
 # Add Grid Lines to a Plot
 
 # Add grid lines to the plot:
-import numpy as np
-import matplotlib.pyplot as plt
 
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
@@ -338,5 +336,22 @@ plt.plot(x, y)
 
 plt.grid()
 
-plt.show() 
+plt.show()
+
+# Specify Which Grid Lines to Display
+
+# Display only grid lines for the x-axis:
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sports Watch Data")
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+plt.plot(x, y)
+
+plt.grid(axis = 'x')
+
+plt.show()
 
