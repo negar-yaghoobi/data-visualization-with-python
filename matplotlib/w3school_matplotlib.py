48b5328ff3d39dac977c68939674be4791bb087d
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 13:22:13 +0330

    add Matplotlib Scatter session

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -388,123 +388,126 @@ import numpy as np
 # plt.show()
 
 
-# Matplotlib Subplot
+# # Matplotlib Subplot
 
-# Display Multiple Plots
-# Draw 2 plots:
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
+# # Display Multiple Plots
+# # Draw 2 plots:
+# #plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
 
-plt.subplot(1, 2, 1)
-plt.plot(x,y)
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
 
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
 
-plt.subplot(1, 2, 2)
-plt.plot(x,y)
-plt.show()
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+# plt.show()
 
-# Draw 2 plots on top of each other:
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
+# # Draw 2 plots on top of each other:
+# #plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
 
-plt.subplot(2, 1, 1)
-plt.plot(x,y)
+# plt.subplot(2, 1, 1)
+# plt.plot(x,y)
 
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
 
-plt.subplot(2, 1, 2)
-plt.plot(x,y)
+# plt.subplot(2, 1, 2)
+# plt.plot(x,y)
 
-plt.show()
-
-
-# Draw 6 plots:
-
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-
-plt.subplot(2, 3, 1)
-plt.plot(x,y)
-
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-
-plt.subplot(2, 3, 2)
-plt.plot(x,y)
-
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-
-plt.subplot(2, 3, 3)
-plt.plot(x,y)
-
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-
-plt.subplot(2, 3, 4)
-plt.plot(x,y)
-
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
-
-plt.subplot(2, 3, 5)
-plt.plot(x,y)
-
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
-
-plt.subplot(2, 3, 6)
-plt.plot(x,y)
-
-plt.show()
+# plt.show()
 
 
-# Title
-# 2 plots, with titles:
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
+# # Draw 6 plots:
 
-plt.subplot(1, 2, 1)
-plt.plot(x,y)
-plt.title("SALES")
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
 
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
+# plt.subplot(2, 3, 1)
+# plt.plot(x,y)
 
-plt.subplot(1, 2, 2)
-plt.plot(x,y)
-plt.title("INCOME")
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
 
-plt.show()
+# plt.subplot(2, 3, 2)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(2, 3, 3)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(2, 3, 4)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(2, 3, 5)
+# plt.plot(x,y)
+
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(2, 3, 6)
+# plt.plot(x,y)
+
+# plt.show()
 
 
-# Super Title
-# Add a title for the entire figure:
+# # Title
+# # 2 plots, with titles:
+# #plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
 
-#plot 1:
-x = np.array([0, 1, 2, 3])
-y = np.array([3, 8, 1, 10])
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
+# plt.title("SALES")
 
-plt.subplot(1, 2, 1)
-plt.plot(x,y)
-plt.title("SALES")
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
 
-#plot 2:
-x = np.array([0, 1, 2, 3])
-y = np.array([10, 20, 30, 40])
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+# plt.title("INCOME")
 
-plt.subplot(1, 2, 2)
-plt.plot(x,y)
-plt.title("INCOME")
+# plt.show()
 
-plt.suptitle("MY SHOP")
-plt.show()
+
+# # Super Title
+# # Add a title for the entire figure:
+
+# #plot 1:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([3, 8, 1, 10])
+
+# plt.subplot(1, 2, 1)
+# plt.plot(x,y)
+# plt.title("SALES")
+
+# #plot 2:
+# x = np.array([0, 1, 2, 3])
+# y = np.array([10, 20, 30, 40])
+
+# plt.subplot(1, 2, 2)
+# plt.plot(x,y)
+# plt.title("INCOME")
+
+# plt.suptitle("MY SHOP")
+# plt.show()
+
+
+# Matplotlib Scatter
