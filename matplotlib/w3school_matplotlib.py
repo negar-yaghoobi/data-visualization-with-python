ba8e234b28810e5062c952a0bdf7a8abb75db1d4
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 12:58:56 +0330

    add Matplotlib Subplot session

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -320,69 +320,72 @@ import numpy as np
 # plt.show()
 
 
-# Matplotlib Adding Grid Lines
-# Add Grid Lines to a Plot
+# # Matplotlib Adding Grid Lines
+# # Add Grid Lines to a Plot
 
-# Add grid lines to the plot:
+# # Add grid lines to the plot:
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
 
-plt.plot(x, y)
+# plt.plot(x, y)
 
-plt.grid()
+# plt.grid()
 
-plt.show()
+# plt.show()
 
-# Specify Which Grid Lines to Display
+# # Specify Which Grid Lines to Display
 
-# Display only grid lines for the x-axis:
+# # Display only grid lines for the x-axis:
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
 
-plt.plot(x, y)
+# plt.plot(x, y)
 
-plt.grid(axis = 'x')
+# plt.grid(axis = 'x')
 
-plt.show()
-
-
-# Display only grid lines for the y-axis:
-
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
-
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
-
-plt.plot(x, y)
-
-plt.grid(axis = 'y')
-
-plt.show()
+# plt.show()
 
 
-# Set Line Properties for the Grid
+# # Display only grid lines for the y-axis:
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
 
-plt.plot(x, y)
+# plt.plot(x, y)
 
-plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+# plt.grid(axis = 'y')
 
-plt.show()
+# plt.show()
+
+
+# # Set Line Properties for the Grid
+
+# x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+# y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+# plt.title("Sports Watch Data")
+# plt.xlabel("Average Pulse")
+# plt.ylabel("Calorie Burnage")
+
+# plt.plot(x, y)
+
+# plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+
+# plt.show()
+
+
+# Matplotlib Subplot
