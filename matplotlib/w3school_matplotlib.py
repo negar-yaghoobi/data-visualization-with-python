82418520bf7a7ca50d3d17c003fdda50bca2483b
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 13:19:56 +0330

    add example about Matplotlib Subplot : Add a title for the entire figure

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -485,3 +485,26 @@ plt.plot(x,y)
 plt.title("INCOME")
 
 plt.show()
+
+
+# Super Title
+# Add a title for the entire figure:
+
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+plt.title("SALES")
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.title("INCOME")
+
+plt.suptitle("MY SHOP")
+plt.show()
