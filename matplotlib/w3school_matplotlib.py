d86803bfcada86b1be4d508f4b84174bfa4c3bd7
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 13:05:01 +0330

    add example about Matplotlib Subplot : draw 2 plot

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -389,3 +389,20 @@ import numpy as np
 
 
 # Matplotlib Subplot
+
+# Display Multiple Plots
+# Draw 2 plots:
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.show()
