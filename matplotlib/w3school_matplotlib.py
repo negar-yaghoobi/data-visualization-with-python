17571125096d571e75e3f28aed20d3c646e2641a
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 13:12:18 +0330

    add example about Matplotlib Subplot : Draw 2 plots on top of each other

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -406,3 +406,20 @@ y = np.array([10, 20, 30, 40])
 plt.subplot(1, 2, 2)
 plt.plot(x,y)
 plt.show()
+
+# Draw 2 plots on top of each other:
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 1, 1)
+plt.plot(x,y)
+
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 1, 2)
+plt.plot(x,y)
+
+plt.show()
