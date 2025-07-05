c4947325c45a0edca277f1016cf09cc9501d918d
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-05 17:43:59 +0330

    add example about Combine Color Size and Alpha

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -607,3 +607,18 @@ sizes = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
 plt.scatter(x, y, s=sizes, alpha=0.5)
 
 plt.show()
+
+
+# Combine Color Size and Alpha
+
+# Create random arrays with 100 values for x-points, y-points, colors and sizes:
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = 10 * np.random.randint(100, size=(100))
+
+plt.scatter(x, y, c=colors, s=sizes, alpha=0.5, cmap='nipy_spectral')
+
+plt.colorbar()
+
+plt.show()
