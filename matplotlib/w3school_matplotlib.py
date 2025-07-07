d481638831adba63008ad2029095b0f95dba4cda
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-07 07:08:59 +0330

    add example4 about bar color with hexadecimal color

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -674,3 +674,11 @@ y = np.array([3, 8, 1, 10])
 
 plt.bar(x, y, color = "hotpink")
 plt.show()
+
+# Color Hex
+# Draw 4 bars with a beautiful green color:
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "#4CAF50")
+plt.show()
