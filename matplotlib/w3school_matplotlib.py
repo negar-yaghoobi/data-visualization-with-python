1912e88c679003ca090c868a387d41f8d8078292
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-07 07:10:13 +0330

    add example about bar width

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -682,3 +682,12 @@ y = np.array([3, 8, 1, 10])
 
 plt.bar(x, y, color = "#4CAF50")
 plt.show()
+
+
+# Bar Width
+# Draw 4 very thin bars:
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1)
+plt.show()
