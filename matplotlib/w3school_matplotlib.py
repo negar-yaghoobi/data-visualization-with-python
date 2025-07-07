7a9368855856f714c734f3e3382cf6b6cf111606
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-07 11:31:59 +0330

    add example about add a legend : a list of explanation for each wedge

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -766,4 +766,14 @@ mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
 mycolors = ["black", "hotpink", "b", "#4CAF50"]
 
 plt.pie(y, labels = mylabels, colors = mycolors)
+plt.show()
+
+# Legend
+# add a legend
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels)
+plt.legend()
 plt.show() 
