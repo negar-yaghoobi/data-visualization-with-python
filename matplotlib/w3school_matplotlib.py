5714824e803d39ff2d74ad21ad54c48f099acad9
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-07 07:05:04 +0330

    add example2 about bar color

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -658,3 +658,11 @@ y = np.array([3, 8, 1, 10])
 
 plt.bar(x, y, color = "red")
 plt.show()
+
+
+# example2
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y, color = "red")
+plt.show()
