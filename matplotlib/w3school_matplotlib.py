5f4332b7c5adead7fe28b5bdc7e4cc44e96dba16
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-07 11:20:53 +0330

    add example about add shadow to pie chart

--- matplotlib/w3school_matplotlib.py
+++ matplotlib/w3school_matplotlib.py
@@ -750,3 +750,11 @@ myexplode = [0.2, 0, 0, 0]
 
 plt.pie(y, labels = mylabels, explode = myexplode)
 plt.show()
+
+# Shadow
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode, shadow = True)
+plt.show()
