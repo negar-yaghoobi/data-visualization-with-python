67ece6f6869a7b06e1872605b7ae64a45055d6b3
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-17 14:22:26 +0330

    view all the dataset in seaborn

--- seaborn/seaborn_tutorialspoint.py
+++ seaborn/seaborn_tutorialspoint.py
@@ -5,3 +5,8 @@ import seaborn as sb
 # Seaborn for plotting and styling
 df = sb.load_dataset('tips')
 print(df.head())
+
+
+# --------------------------------------------
+# To view all the available data sets in the Seaborn library
+print (sb.get_dataset_names())
